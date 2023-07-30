894d4a4ca07373a7504135fc6c15c4c747a7d32e
Anya-gh <Aniket.Singh@warwick.ac.uk>
2023-07-30 11:43:25 +0100

    setup tailwind, dev scripts and routing

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
+import { BrowserRouter } from 'react-router-dom'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter basename={process.env.NODE_ENV === 'production' ? '/blog' : '/'}>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
 )
