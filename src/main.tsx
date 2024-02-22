15bf63b1e2d77c96e27aef16fe7d51bc182c7c21
Anya-gh <Aniket.Singh@warwick.ac.uk>
2024-02-22 13:05:26 +0000

    removed typo

--- src/main.tsx
+++ src/main.tsx
@@ -9,5 +9,5 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
     <BrowserRouter basename={process.env.NODE_ENV === 'production' ? '/blog' : '/'}>
       <App />
     </BrowserRouter>
-  </React.StrictMode>,
+  </React.StrictMode>
 )
